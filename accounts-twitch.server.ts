ec6b6f451fcbd29f546303426ba726c53e8bfa7b
Tom Brückner <tom@marblear.com>
2025-03-17 07:58:34 +0100

    corrected auto-publish fields key

--- accounts-twitch.server.ts
+++ accounts-twitch.server.ts
@@ -5,12 +5,12 @@ Accounts.addAutopublishFields({
   forLoggedInUser: TwitchOAuth.whitelistedFields
     .concat(['accessToken', 'expiresAt'])
     .map(
-      (subfield: string) => `services.apple.${subfield}` // don't publish refresh token
+      (subfield: string) => `services.twitch.${subfield}` // don't publish refresh token
     ),
 
   // even with autopublish, no legitimate web app should be
   // publishing all users' emails
   forOtherUsers: TwitchOAuth.whitelistedFields
     .filter((field: string) => field !== 'email' && field !== 'verified_email')
-    .map((subfield: string) => `services.apple.${subfield}`)
+    .map((subfield: string) => `services.twitch.${subfield}`)
 });
