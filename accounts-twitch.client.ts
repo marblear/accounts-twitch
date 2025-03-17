7c8fe8403d428726ccac7209261ac704c752a1ff
Tom Brückner <tom@marblear.com>
2025-03-17 09:23:17 +0100

    support requestCredential() without options

--- accounts-twitch.client.ts
+++ accounts-twitch.client.ts
@@ -2,7 +2,7 @@ import { TwitchOAuth } from 'meteor/marblear:twitch-oauth';
 import TwitchAccounts from './TwitchAccounts';
 import { Accounts, CredentialRequestCompleteCallback, LoginOptions } from './types';
 
-const loginWithTwitch = async (options: LoginOptions, callback: CredentialRequestCompleteCallback) => {
+const loginWithTwitch = async (options: LoginOptions | CredentialRequestCompleteCallback, callback: CredentialRequestCompleteCallback) => {
   // support a callback without options
   if (!callback && typeof options === 'function') {
     callback = options;
